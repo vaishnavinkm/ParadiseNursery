89f9b9f7986b8e8a29e793215b3f48beced9d51d
vaishnavinkm <vaishnavinikam1999@gmail.com>
2025-02-15 13:37:22 -0500

    feat: initialize cart state in Redux, add addItem action, and retrieve item quantity from store

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,11 +2,15 @@ import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
+import {useSelector, useDispatch } from 'react-redux';
 
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const cart = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+
 
     const plantsArray = [
         {
